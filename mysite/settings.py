03b561770a7b3b8c102c8fa3120d8375ed8b562f
edwinwu(吴嘉龙) <edwinwu@tencent.com>
2016-06-06 18:00:57 +0800

    second

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,17 +73,32 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        # 默认db，主要记录主要和公用信息
+         'default': {
+            'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': '',                    # Or path to database file if using sqlite3.
+            'USER': '',                    #
+            'PASSWORD': '',               # KCFyzy68om
+            'HOST': '',                 # 10.6.224.220
+            'PORT': '',                         # Set to empty string for default. Not used with sqlite3.
+            #'OPTIONS':{
+            #    'init_command':'SET storage_engine=INNODB',
+            #},
+        }
 }
 
 
